72eb6d63fc9e04288c178280ccc588315c25b2f2
Miranto <hei.finoana.5@gmail.com>
2025-09-04 17:49:22 +0300

    feat: add middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; 
+
+  if (!token) return res.status(401).json({ message: 'Access denied, token missing!' });
+
+  jwt.verify(token, 'defaultSecretCode', (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token invalid or expired' });
+
+    req.user = user;
+    next();
+  });
+};
