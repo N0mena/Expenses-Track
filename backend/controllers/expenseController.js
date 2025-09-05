3c216783f7ca733d48f80c6d51393498c5386873
Miranto <hei.finoana.5@gmail.com>
2025-09-05 12:08:38 +0300

    feat: add all/expense

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,27 +1,41 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
 
 export const createExpense = async (req,res) => {
     try {
         const { title, amount,type, date ,categoryId , description , startDate, endDate, receipt } = req.body;
 
         if( !title || !amount ){
-            return res.status(400).json({message: "require title and amount"})
+            return res.status(400).json({message: "title and amount are required"})
         }
-        const expenese = new Expense({
+        const expense = await prisma.expense.create({
             title,
             type,
-            date,
+            amount: parseFloat(amount),
+            date: date ? new Date(date): new date(),
             categoryId,
             description,
-            startDate,
-            endDate,
+            startDate: startDate ? new startDate(startDate): null,
+            endDate: endDate ? new endDate(endDate): null,
             receipt
         })
 
-        const savedExpense = await expenese.save();
+        const savedExpense = await expense.save();
         res.status(201).json(savedExpense);
 
     } catch (error) {
         res.status(500).json({message:"Error servor", error: error.message});      
     }
 };
+
+export const getExpense = async (req,res) => {
+    try {
+        const expense = await prisma.expense.findMany({
+            orderBy: { date: "desc"}
+        })
+        return res.json(expense)
+    } catch (error) {
+        res.status(500).json({message: "server error",error: error.message})   
+    }
+}
