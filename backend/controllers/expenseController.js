98e3e93b79dd716b6e49a882298baaae6a932f54
Miranto <hei.finoana.5@gmail.com>
2025-09-09 18:49:45 +0300

    feat: search expense id

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -11,14 +11,14 @@ export const createExpense = async (req,res) => {
         }
         const expense = await prisma.expense.create({
             title,
-            type,
+            type: type ==='reccuring'? 'recurring': 'one_time',
             amount: parseFloat(amount),
-            date: date ? new Date(date): new date(),
+            date:  new Date(date),
             categoryId,
             description,
             startDate: startDate ? new startDate(startDate): null,
             endDate: endDate ? new endDate(endDate): null,
-            receipt
+            receipt,
         })
 
         const savedExpense = await expense.save();
@@ -31,11 +31,37 @@ export const createExpense = async (req,res) => {
 
 export const getExpense = async (req,res) => {
     try {
-        const expense = await prisma.expense.findMany({
-            orderBy: { date: "desc"}
-        })
-        return res.json(expense)
+        const { start,end,category,type} = req.query;
+
+        const userId = req.user.id;
+        const where = { userId };
+
+        if( start || end){
+            where.date= {};
+            if (start) where.date.gte = new Date(start);
+            if (end) where.date.lte = new Date(end)
+        }
     } catch (error) {
         res.status(500).json({message: "server error",error: error.message})   
     }
+}
+
+export const getExpenseById = async (req,res) => {
+    try {
+        const { id } = req.params;
+        const userId = req.user.id;
+        const expense = await prisma.expense.findFirst({
+            where: { id, userId},
+        });
+
+        if(!expense){
+            return res.status(404).json({ message: "Expense not found"})
+        }
+         res.json(expense);
+
+    } catch (error) {
+res.status(500).json({ message: "Server error", error: error.message });
+
+    }
+    
 }
