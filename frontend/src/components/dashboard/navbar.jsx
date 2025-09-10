7789cd70859fae8b9bd9cadb554f63944f85432e
N0mena <alida.rajaonarivelo@gmail.com>
2025-09-10 18:40:05 +0300

    fix: fix navbar buttons in dashboard

--- frontend/src/components/dashboard/navbar.jsx
+++ frontend/src/components/dashboard/navbar.jsx
@@ -1,8 +1,15 @@
-// src/components/UI/Navbar.jsx
 import Link from "next/link";
+import { useState } from "react";
+import NewIncome from "./newIncome";
+import NewExpense from "./newExpense";
+
 
 const Navbar = () => {
+  const [isIncomeOpen, setIsIncomeOpen] = useState(false);
+  const [isExpenseOpen, setIsExpenseOpen] = useState(false);
+
   return (
+    <div>
     <nav className="bg-white shadow-md px-6 py-3 flex justify-between items-center sticky top-0 z-50">
       <div className="text-xl font-bold text-pink-600">Expense Tracker</div>
 
@@ -14,18 +21,37 @@ const Navbar = () => {
         </Link>
 
         <Link href="/newIncome">
-          <p className="px-3 py-1 rounded hover:bg-green-100 transition">
+          <p
+            className="px-3 py-1 rounded hover:bg-green-100 transition"
+            onClick={() => setIsIncomeOpen(true)}
+          >
             Add Income
           </p>
         </Link>
 
         <Link href="/newExpense">
-          <p className="px-3 py-1 rounded hover:bg-red-100 transition">
+          <p
+            className="px-3 py-1 rounded hover:bg-red-100 transition"
+            onClick={() => setIsExpenseOpen(true)}
+          >
             Add Expense
           </p>
         </Link>
       </div>
     </nav>
+     {isIncomeOpen && (
+        <NewIncome
+          isOpen={isIncomeOpen}
+          onClose={() => setIsIncomeOpen(false)} 
+        />
+      )}
+     {isExpenseOpen && (
+        <NewExpense
+          isOpen={isExpenseOpen}
+          onClose={() => setIsExpenseOpen(false)} 
+        />
+      )}
+    </div>
   );
 };
 
