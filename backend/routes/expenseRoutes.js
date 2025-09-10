a6b142872b9130a01a4250e42399f8374e2a1ed7
Miranto <hei.finoana.5@gmail.com>
2025-09-10 14:37:28 +0300

    feat: delete expenseroute

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,10 +1,16 @@
-import { Router } from "express";
-import {createExpense, getExpense} from "../controllers/expenseController.js";
+import e, { Router } from "express";
+import {createExpense, deleteExpense, getExpense, getExpenseById, updateExpense} from "../controllers/expenseController.js";
 
 const expenseRoutes = Router();
 
 expenseRoutes.get("/", getExpense);
 
 expenseRoutes.post("/", createExpense);
+
+expenseRoutes.get("/:id",getExpenseById);
+
+expenseRoutes.delete("/:id",deleteExpense);
+
+expenseRoutes.update("/:id",updateExpense);
 
 export default expenseRoutes;
