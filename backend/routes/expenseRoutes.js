4b70531c0c2bbddd26f50b512612f978a709855d
Miranto <hei.finoana.5@gmail.com>
2025-09-11 20:11:08 +0300

    fix: all backend

---
+++ backend/routes/expenseRoutes.js
@@ -0,0 +1,16 @@
+import e, { Router } from "express";
+import {createExpense, deleteExpense, getExpense, getExpenseById, updateExpense} from "../controllers/expenseController.js";
+
+const expenseRoutes = Router();
+
+expenseRoutes.get("/", getExpense);
+
+expenseRoutes.post("/", createExpense);
+
+expenseRoutes.get("/:id",getExpenseById);
+
+expenseRoutes.delete("/:id",deleteExpense);
+
+expenseRoutes.update("/:id",updateExpense);
+
+export default expenseRoutes;
